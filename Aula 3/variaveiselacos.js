c31f11eeddd7c3f11ad552dc3fb1871aa3441d47
Daniel <danielscn2022@gmail.com>
2024-04-10 08:39:14 -0300

    Aula variáveis

---
+++ Aula 3/variaveiselacos.js
@@ -0,0 +1,21 @@
+let n =1 
+let t = "oi"
+
+console.log (typeof n )
+console.log (typeof t )
+console.log (typeof t, typeof n )
+
+let a = [0, 1, 1 ,2 ,3 ,5,7,5,4 ]
+console.log (typeof  a )
+let objeto = {nome: "Danie", idade: "Arvore Bonita"}
+console.log(typeof objeto)
+//objeto nome é otribulo "" é informação
+
+console.log (a)
+console.log (a[6])
+
+a.forEach  ((el) =>
+    console.log(`Elemento: ${el}`)
+)
+console.log(objeto)
+console.log(objeto.nome, "é uma", objeto.idade)
